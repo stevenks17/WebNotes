41489f6d1d940cd0c37e9c4688ec1d51ce2c58a9
stevenks17 <github email address>
2021-05-07 14:25:35 -0400

    customized notes show

--- app/admin/notes.rb
+++ app/admin/notes.rb
@@ -1,5 +1,36 @@
 ActiveAdmin.register Note do
 
+
+  permit_params :title, :body, :topic_id
+
+
+  form title: 'WebNotes' do |f|
+    f.inputs 'Note' do 
+      f.input :topic
+      f.input :title
+      f.input :body, as: :quill_editor
+    end
+    f.actions
+  end
+
+  show title: 'Your Note' do 
+    h1 link_to note.title, admin_notes_path
+    h4 link_to note.topic.name, admin_topic_path(note.topic)
+    div(class: 'note-body') do 
+      raw note.body
+    end
+  end
+
+  index do
+    selectable_column
+    column :topic
+    column 'Title' do |note|
+      note.title.presence || truncate(note.body, length: 20) 
+
+    end
+  end
+
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
