7e9cbec1cd087e2e0a93b9172dca714dc93e3bf2
stevenks17 <github email address>
2021-05-07 17:59:35 -0400

    finished dashboard cards

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,30 +3,35 @@ ActiveAdmin.register_page "Dashboard" do
 
   content title: proc { I18n.t("active_admin.dashboard") } do
     div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
+
     end
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
+    columns do 
+      column do
+        panel 'Recent Notes' do
+          ul do
+            Note.order(created_at: :desc).first(5).map do |note|
+              li link_to(note.title, admin_note_path(note))
+            end
+          end
+        end
+      end
+      column do
+        panel 'Info' do
+          para 'Welcome to Web Notes. It is powered by Ruby on Rails and Active Admin'
+        end
+      end
+    end
+    columns do
+      column do
+        panel 'All Topics' do
+          ul do
+            Topic.order(name: :asc).map do |topic|
+              li link_to(topic.name, admin_topic_path(topic))
+            end
+          end
+        end
+      end
+    end
   end # content
 end
