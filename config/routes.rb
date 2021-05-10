febf4da5d9297ce728a8c65cd1cda1555bbda6c7
stevenks17 <github email address>
2021-05-10 12:42:14 -0400

    defaulted to dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
+  root to: 'admin/dashboard#index'
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
