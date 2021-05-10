9eef5696135af7cdb080aaf9fb38e1ebfcf41016
stevenks17 <github email address>
2021-05-10 17:21:12 -0400

    added new if validation for title and boidy

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,6 +1,16 @@
 class Note < ApplicationRecord
   belongs_to :topic
-  validates :title, presence: true
+
   validates_length_of :title, maximum: 30
   validates_length_of :body, maximum: 1000
+
+  validate :title_or_body
+
+  private
+
+  def title_or_body
+   if title.blank? && body.blank?
+      errors[:base] << "Please add a title or message in body to save the note"
+    end
+  end
 end
